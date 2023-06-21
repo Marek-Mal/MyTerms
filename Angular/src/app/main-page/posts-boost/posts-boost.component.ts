5b0f3aa0a5d51d00b7e8124760cf8cd7662334b7
Marek-Mal <79137873+Marek-Mal@users.noreply.github.com>
2023-06-21 18:16:13 +0200

    Update posts-boost.component.ts
    comment

--- Angular/src/app/main-page/posts-boost/posts-boost.component.ts
+++ Angular/src/app/main-page/posts-boost/posts-boost.component.ts
@@ -28,7 +28,7 @@ export class PostsBoostComponent implements OnInit {
     this.refreshTerms()
     
   }
-
+  // gain subscribed date from server to show newest results 
   refreshTerms() {
     this.service.get_boost().subscribe(data => {
       this.collectionSize = data.results.length;
@@ -38,7 +38,7 @@ export class PostsBoostComponent implements OnInit {
         );
     })
   }
-  
+  // modal more info
   moreInfoClick(item:any, longContent:any) {
     this.terms=item;
     this.modalService.open(longContent, { scrollable: true, size: "lg" });
